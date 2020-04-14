44f12cc8abcfba56a451275783af24a04477ab12
G. Vanessa Fotso <vanessuniq@gmail.com>
2020-04-14 05:47:04 +0000

    Automatically backed up by Learn

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -14,6 +14,6 @@ class Genre < ActiveRecord::Base
 
   def all_artist_names
     # return an array of strings containing every musician's name
-    self.artists.map {|artist| artist.name
+    self.artists.map {|artist| artist.name}
   end
 end
