bc6ccc4e3678166a32457a157636833ae4e66f9f
G. Vanessa Fotso <vanessuniq@gmail.com>
2020-04-14 05:44:18 +0000

    Automatically backed up by Learn

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -14,7 +14,6 @@ class Artist < ActiveRecord::Base
 
   def genre_count
     #return the number of genres associated with the artist
-    self.songs.each {|s| genre_number << s.genre}
-    genre_number.count
+    self.genres.count
   end
 end
